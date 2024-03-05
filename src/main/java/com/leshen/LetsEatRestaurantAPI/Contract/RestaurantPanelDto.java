bed32cc3830995b7ce9290c38a5431fd23e79459
kharass <k.haras@wp.pl>
2024-03-05 17:27:20 +0100

    naprawienie stars
    - naprawienie wyświetlania średniej oceny po kliknięciu w konkretną restauracje w aplikacji mobilnej. Po prostu nie zostało zmappowane dlatego nie działało wcześniej

--- src/main/java/com/leshen/LetsEatRestaurantAPI/Contract/RestaurantPanelDto.java
+++ src/main/java/com/leshen/LetsEatRestaurantAPI/Contract/RestaurantPanelDto.java
@@ -3,7 +3,9 @@ package com.leshen.LetsEatRestaurantAPI.Contract;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -53,4 +55,9 @@ public class RestaurantPanelDto {
 
     @Schema(description = "Calculated average rating of atmosphere in restaurant", minimum = "1", maximum = "5")
     private Double averageAtmosphere;
+
+    @Setter
+    @Getter
+    @Schema(description = "Calculated rating of restaurant", minimum = "1", maximum = "5")
+    private double stars;
 }
